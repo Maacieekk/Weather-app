624057bb3745ef7d80303e46ac56982586ad995e
Maacieekk <nie1wiem2@gmail.com>
2022-07-05 18:34:29 +0200

    savegame :)

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -9,20 +9,44 @@ export default {
   data() {
     return {
       localeInfo: 'Chicago', // domyślna lokalizacja
-    };
-  },
+    }
+  }
   // "sprawdzaj / patrz" element, którego nazwa jest nazwą funkji
   watch : {
-    // patrz na zmienną localInfo 
+    // patrz na zmienną localInfo
     localeInfo(newVal){
       console.log('newData: ', newVal)
     }
-  },
+  }
+  //
+  methods : {
+getlaton(){
+  console.log('getin lat lon')
+  fetch('http://api.openweathermap.org/geo/1.0/direct?q='+this.localeInfo'&appid=04d03c358e8933ac6823da54c340c97b'
+  )
+
+  .then((dt) => dt.json())
+  .then((dt) => {
+    console.log('lat: ', dt[0].lat)
+    console.log('lon: ', dt[0].lat)
+    this.getweather(dt[0].lat, dt[0].lo)
+  })
+  }
+}
+
   // mouted - funkcja wywołująca się po "zamontowaniu komponentu"
   mounted() {
+    this.getlaton()
     // console.log('hello');
-  },
-};
+  }
+  getweather(lat, lon){
+    fetch()
+    .then(dt => dt.json())
+    .then((dt) =>{
+      console.log()
+    })
+  }
+}
 </script>
 
 <style>
